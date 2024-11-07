1b14b937e1ffe76a81840b41fb68036027e261ef
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-07 08:56:46 +0100

    Auth dependency does not have to be imported now

--- runner.py
+++ runner.py
@@ -14,7 +14,6 @@ from cryptography import x509
 import ols.app.models.config as config_model
 from ols import constants
 from ols.utils import tls
-from ols.utils.auth_dependency import K8sClientSingleton
 from ols.utils.logging import configure_logging
 
 
