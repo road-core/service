ecd9e83d751d21b8bf8831b8a88742fe86f6d735
Pavel Tišnovský <ptisnovs@redhat.com>
2025-02-10 09:49:39 +0100

    Sync with downstream (#372)

--- tests/integration/test_project_version_consistency.py
+++ tests/integration/test_project_version_consistency.py
@@ -26,9 +26,7 @@ def read_version_from_pyproject():
     # the PDM tool itself is able to retrieve the version, even if the version
     # is generated dynamically
     completed = subprocess.run(  # noqa: S603
-        ["pdm", "show", "--version"],  # noqa: S607
-        capture_output=True,
-        check=True,
+        ["pdm", "show", "--version"], capture_output=True, check=True  # noqa: S607
     )
     return completed.stdout.decode("utf-8").strip()
 
