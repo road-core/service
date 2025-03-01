fed694ff7bde88ce367f28a05589e1d7b2ad3790
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-01 15:56:16 +0100

    Better quota exceed error type

--- ols/src/quota/quota_exceed_error.py
+++ ols/src/quota/quota_exceed_error.py
@@ -4,17 +4,28 @@
 class QuotaExceedError(Exception):
     """Any exception that can occur when user does not have enough tokens available."""
 
-    def __init__(self, subject_id: str, subject_type: str, available: int, needed: int):
+    def __init__(self, subject_id: str, subject_type: str, available: int, needed: int = 0):
         """Construct exception object."""
-        match subject_type:
-            case "u":
-                message = f"User {subject_id} has {available} tokens, but {needed} tokens are needed"  # noqa: E501
-            case "c":
-                message = (
-                    f"Cluster has {available} tokens, but {needed} tokens are needed"
-                )
-            case _:
-                message = f"Unknown subject {subject_id} has {available} tokens, but {needed} tokens are needed"  # noqa: E501
+        if needed == 0 and available <= 0:
+            match subject_type:
+                case "u":
+                    message = f"User {subject_id} has no available tokens"
+                case "c":
+                    message = (
+                        f"Cluster has no available tokens"
+                    )
+                case _:
+                    message = f"Unknown subject {subject_id} has no available tokens"
+        else:
+            match subject_type:
+                case "u":
+                    message = f"User {subject_id} has {available} tokens, but {needed} tokens are needed"  # noqa: E501
+                case "c":
+                    message = (
+                        f"Cluster has {available} tokens, but {needed} tokens are needed"
+                    )
+                case _:
+                    message = f"Unknown subject {subject_id} has {available} tokens, but {needed} tokens are needed"  # noqa: E501
 
         # call the base class constructor with the parameters it needs
         super().__init__(message)
